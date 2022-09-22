27d6b85b2700e6b56812db4f4f5e148e1430ef48
Yip <73788307+tmm02@users.noreply.github.com>
2022-09-22 10:58:45 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.myapplication1
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.myapplication1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var btnIntent: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        btnIntent = findViewById(R.id.btn_intent)
+
+        btnIntent.setOnClickListener(this)
+    }
+    override fun onClick(v: View){
+        when(v.id){
+            R.id.btn_intent ->{
+                val intentBiasa = Intent(this@MainActivity, MainActivityjava::class.java)
+                startActivity(intentBiasa)
+            }
+        }
+    }
+}
